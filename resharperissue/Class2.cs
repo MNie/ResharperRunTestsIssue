b5793b3ce9934193996c55854ff63ddb5d1e72c3
Michal Niegrzybowski <michal.niegrzybowski@ihs.com>
2017-12-27 18:27:59 +0100

    init

---
+++ resharperissue/Class2.cs
@@ -0,0 +1,19 @@
+﻿using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
+// ReSharper disable InconsistentNaming
+
+namespace resharperissue
+{
+    [Subject("2")]
+    public class Class2 : Observes
+    {
+        private static int sut;
+
+        private Establish that = () =>
+            sut = 10;
+
+        private It should_equal_10 = () => sut.ShouldEqual(10);
+        private It should_not_equal_11 = () => sut.ShouldNotEqual(11);
+        private It should_not_equal_12 = () => sut.ShouldNotEqual(12);
+    }
+}
